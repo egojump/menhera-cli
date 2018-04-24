de766ce20d7998322ff7f5493a86227dae5cb65f
nano <ncysatnaf@gmail.com>
2018-04-24 13:52:28 +0800

    use new version menhera

--- packages/menhera-cli/cli.js
+++ packages/menhera-cli/cli.js
@@ -4,20 +4,17 @@ import CLI from "../core";
 import * as messages from "./messages";
 import * as commands from "./commands";
 
-export default Menhera({
+export default Menhera.$use({
   _mount: {
     CLI
   },
   CLI: {
     messages,
-    commands
-  }
-}).$use({
-  CLI: {
+    commands,
     config: {
       name: "mhr",
       version: "0.0.1",
       start: true
     }
   }
-});
+})
