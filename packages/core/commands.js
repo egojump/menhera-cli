f84807d1d9a3781babcca2e83f72046fab691c1e
cirnoo <ncysatnaf@gmail.com>
2018-04-29 10:33:25 +0800

    update example message alert

--- packages/core/commands.js
+++ packages/core/commands.js
@@ -1,4 +1,5 @@
 import { EVENTS } from "menhera";
+import chalk from "chalk";
 
 export const examples = {
   desc: "Examples",
@@ -17,6 +18,8 @@ export const examples = {
 
     let val = examples[example];
     let target = val.split(" ");
+
+    console.log(chalk.grey(`>>> ${name} ${val}`));
 
     _.$use({ [name]: { config: { target, start: true } } });
   }
